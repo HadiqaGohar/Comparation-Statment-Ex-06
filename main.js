572888ec8722de332d23623a643a4bb1ed35e0f1
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:39:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//COMPARATION OPERATOR
+//RAIN
+var isRaining = true; //use true and false
+if (isRaining) {
+    console.log("Wear a raincoat.");
+}
+else {
+    console.log("Wear sunglasses.");
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+//COMPARATION OPERATOR
+//RAIN
+let isRaining = true;  //use true and false
+if (isRaining) {
+    console.log("Wear a raincoat.");
+} else {
+    console.log ("Wear sunglasses.");
+}
